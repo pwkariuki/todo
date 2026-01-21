47eb0776fee56fe7a7326a2b8c60d541035570e9
Patrick Kariuki <pkariuk1@jhu.edu>
2026-01-21 18:49:51 -0500

    capturing input from stdin to support adding items by piping

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 	"todo"
 )
 
@@ -17,7 +20,7 @@ func main() {
 	}
 
 	// Parsing command line flags
-	task := flag.String("task", "", "Task to be included in the ToDo list")
+	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 
@@ -26,7 +29,7 @@ func main() {
 	// Define an items list
 	l := &todo.List{}
 
-	// Use the Get command to read to do items from filr
+	// Use the Get command to read to do items from file
 	if err := l.Get(todoFileName); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -48,9 +51,15 @@ func main() {
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
-	case *task != "":
-		// Add the task
-		l.Add(*task)
+	case *add:
+		// When any arguments (excluding flags) are provided,
+		// they will be used as the new task
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
 
 		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
@@ -62,4 +71,24 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Invalid option")
 		os.Exit(1)
 	}
+}
+
+// getTask function decides where to gte the description for a new
+// task from: arguments or STDIN
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+
+	return s.Text(), nil
 }
